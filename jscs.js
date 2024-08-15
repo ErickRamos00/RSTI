a8fe88a70ab25f7213a6063cabb3f3a105dda2d0
ErickRamos00 <azevedoerick@yahoo.com.br>
2024-08-15 16:49:53 -0300

    novo arquivo

---
+++ jscs.js
@@ -0,0 +1,11 @@
+const amorzinhos = ["Renatinha Rodrigues", "Erick", "Eric", "Mateus", "Wellington", "Felipe", "Ana", "Leo"]
+
+class Amor {
+constructor(array){
+    this.array = array;
+}
+ 
+ amores (array){
+    return array;
+ }
+}
